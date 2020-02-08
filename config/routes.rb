60c6e629f745ffa3bc4855b73f28a12c3beb3cbd
tani <taniguchitakayasu@tani-Mac-book-pro.local>
2020-02-08 15:05:56 +0900

    use_resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get '/users', to: 'users#index'
-  get '/users/new', to: 'users#new'
-  post '/users', to: 'users#create'
-  # 以下のルーティグを追記
-  get '/users/:id/edit', to: 'users#edit'
-  patch '/users/:id', to: 'users#update'
-  delete '/users/:id', to: 'users#destroy'
+  # 以下のルーティングを定義
+  resources :users
 end
